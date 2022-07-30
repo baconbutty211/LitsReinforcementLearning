5e02d1dac84f15ebdae7afaa0d1637e3eaeeaec4
Jake <jleiser211@gmail.com>
2022-07-30 14:24:45 +0100

    Implements a forms program of LiTS for a user to play against an AI.

---
+++ LitsForms/EnvironmentForm.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using LitsReinforcementLearning;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LitsForms
+{
+    public partial class EnvironmentForm : Form
+    {
+        protected LitsReinforcementLearning.Environment environment = new LitsReinforcementLearning.Environment();
+
+        private static int size = LitsReinforcementLearning.Environment.size;
+        private Button[] board = new Button[size];
+        private Stack<KeyValuePair<int, LitsReinforcementLearning.Environment>> previousActionsStack = new Stack<KeyValuePair<int, LitsReinforcementLearning.Environment>>(); // Keeps track of the previous environment states and the accompanying action the is applied next.
+
+        public EnvironmentForm()
+        {
+            InitializeComponent();
+            
+            environment.boardChanged += Environment_BoardChanged;
+            
+            validActionsList.MouseDoubleClick += ValidActionsList_MouseDoubleClick;
+            validActionsList.KeyDown += ValidActionsList_KeyDown;
+
+            previousActionsList.MouseDoubleClick += PreviousActionsList_MouseDoubleClick;
+            previousActionsList.KeyDown += PreviousActionsList_KeyDown;
+        }
+
+        private void EnvironmentForm_Load(object sender, EventArgs e)
+        {
+            // Initializes Board
+            int boardSize = (int)Math.Sqrt(size);
+            for (int i = 0; i < size; i++)
+            {
+                board[i] = new Button();
+                Size size = new Size(50,50);
+                board[i].Size = size;
+                int x = 3 + (size.Width * (i % boardSize));
+                int y = 3 + (size.Height * (i / boardSize));
+                board[i].Location = new Point(x, y);
+                boardPanel.Controls.Add(board[i]);
+            }
+            resultLab.Text = "";
+
+            environment.Reset();
+        }
+        private void Environment_BoardChanged(LitsReinforcementLearning.Environment.Tile[] board)
+        {
+            // Updates the board buttons
+            for (int i = 0; i < board.Length; i++)
+            {
+                string text = " "; // Default text
+                Color backColour = SystemColors.Control; // Default colour
+                switch (board[i])
+                {
+                    case LitsReinforcementLearning.Environment.Tile._:
+                        break;
+                    case LitsReinforcementLearning.Environment.Tile.O:
+                        text = board[i].ToString();
+                        break;
+                    case LitsReinforcementLearning.Environment.Tile.X:
+                        text = board[i].ToString();
+                        break;
+                    case LitsReinforcementLearning.Environment.Tile.L:
+                        text = board[i].ToString();
+                        backColour = Color.Blue;
+                        break;
+                    case LitsReinforcementLearning.Environment.Tile.I:
+                        text = board[i].ToString();
+                        backColour = Color.Green;
+                        break;
+                    case LitsReinforcementLearning.Environment.Tile.T:
+                        text = board[i].ToString();
+                        backColour = Color.Magenta;
+                        break;
+                    case LitsReinforcementLearning.Environment.Tile.S:
+                        text = board[i].ToString();
+                        backColour = Color.Yellow;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+                this.board[i].Text = text;
+                this.board[i].BackColor = backColour;
+            }
+
+            if (environment.isDone)
+                resultLab.Text = environment.GetResult();
+            else
+                resultLab.Text = "";
+
+            // Updates the valid actions ListBox
+            validActionsList.Items.Clear();
+            foreach (LitsReinforcementLearning.Action act in environment.validActions)
+                validActionsList.Items.Add(act);
+
+            
+            RandomActionBtn.Enabled = !environment.isDone;
+        }
+        
+        private void ResetEnvironmentBtn_Click(object sender, EventArgs e)
+        {
+            environment.Reset();
+            previousActionsList.Items.Clear();
+            previousActionsStack.Clear();
+            RandomActionBtn.Enabled = true;
+        }
+        private void RandomActionBtn_Click(object sender, EventArgs e)
+        {
+            LitsReinforcementLearning.Action randAct = environment.GetRandomAction();
+            ApplyAction(randAct);
+        }
+        
+        private void ValidActionsList_MouseDoubleClick(object sender, EventArgs e)
+        {
+            string act = validActionsList.SelectedItem.ToString();
+            int actId = int.Parse(act.Split(')')[0]);
+            LitsReinforcementLearning.Action action = LitsReinforcementLearning.Action.GetAction(actId);
+
+            ApplyAction(action);
+        }
+        private void ValidActionsList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (validActionsList.SelectedItem != null)
+                validActionsList.SelectedItem = null;
+        }
+
+        private void PreviousActionsList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string act = previousActionsList.SelectedItem.ToString();
+            int actId = int.Parse(act.Split(')')[0]);
+            LitsReinforcementLearning.Action action = LitsReinforcementLearning.Action.GetAction(actId);
+
+            KeyValuePair<int, LitsReinforcementLearning.Environment> state;
+            do
+            {
+                state = previousActionsStack.Pop();
+                previousActionsList.Items.RemoveAt(previousActionsList.Items.Count - 1);
+            }
+            while (state.Key != actId);
+
+            environment.boardChanged -= Environment_BoardChanged;
+            environment = state.Value;
+            environment.boardChanged += Environment_BoardChanged;
+
+            ApplyAction(action);
+        }
+        private void PreviousActionsList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (previousActionsList.SelectedItem != null)
+                previousActionsList.SelectedItem = null;
+        }
+
+        protected void ApplyAction(LitsReinforcementLearning.Action action) 
+        {
+            KeyValuePair<int, LitsReinforcementLearning.Environment> state = new KeyValuePair<int, LitsReinforcementLearning.Environment>(action.Id, environment.Clone());
+            previousActionsStack.Push(state); // Stores the environment before the action is applied, so that when it is recalled the action is applied and the board will update.
+            previousActionsList.Items.Add(action);
+
+            environment.Step(action);
+        }
+    }
+}
---
+++ LitsForms/PlaygroundForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace LitsForms
+{
+    partial class PlaygroundForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AiActionBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AiActionBtn
+            // 
+            this.AiActionBtn.Location = new System.Drawing.Point(811, 713);
+            this.AiActionBtn.Name = "AiActionBtn";
+            this.AiActionBtn.Size = new System.Drawing.Size(146, 49);
+            this.AiActionBtn.TabIndex = 8;
+            this.AiActionBtn.Text = "AI Action";
+            this.AiActionBtn.UseVisualStyleBackColor = true;
+            this.AiActionBtn.Click += new System.EventHandler(this.AiActionBtn_Click);
+            // 
+            // PlaygroundForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1666, 910);
+            this.Controls.Add(this.AiActionBtn);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "PlaygroundForm";
+            this.Text = "PlaygroundForm";
+            this.Controls.SetChildIndex(this.AiActionBtn, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AiActionBtn;
+    }
+}
